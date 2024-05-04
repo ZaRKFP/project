7ea720913fc2db92020220ba95b1be6c4b6cb034
ZaRKFP <75006651+ZaRKFP@users.noreply.github.com>
2024-05-04 21:03:59 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import os
+import CRUD as CRUD
+
+if __name__ == "__main__":
+    sistem_operasi = os.name
+
+    match sistem_operasi:
+        case "posix": os.system("clear")
+        case "nt": os.system("cls")
+
+    print("SELAMAT DATANG DI PROGRAM")
+    print("DATABASE PERPUSTAKAAN")
+    print("=========================")
+
+    # check database itu ada atau tidak
+    CRUD.init_console()
+
+    while(True):
+        match sistem_operasi:
+            case "posix": os.system("clear")
+            case "nt": os.system("cls")
+        
+        print("SELAMAT DATANG DI PROGRAM")
+        print("DATABASE PERPUSTAKAAN")
+        print("=========================")
+
+        print(f"1. Read Data")
+        print(f"2. Create Data")
+        print(f"3. Update Data")
+
+        user_option = input("Masukan opsi: ")
+
+        match user_option:
+            case "1": CRUD.read_console()
+            case "2": CRUD.create_console()
+            case "3": CRUD.update_console()
+
+        is_done = input("Udah Selesai (y/n)? ")
+        if is_done == "y" or is_done == "Y":
+            break
+
+    print("Program berakhir, mksih yh")
